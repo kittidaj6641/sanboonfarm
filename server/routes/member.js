2f97a7903ddf421cda0d7052cd7f4d66075b8138
kittidaj6641 <kittidaj.im@ksu.ac.th>
2025-11-26 23:04:45 +0700

    Update member.js

--- server/routes/member.js
+++ server/routes/member.js
@@ -5,6 +5,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { verifyToken } from "../middleware/auth.js";
+import auth from '../middleware/auth.js'; // ✅ Import auth middleware
+
 
 dotenv.config();
 const router = express.Router();
